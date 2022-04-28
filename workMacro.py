c07668cb45c0377979b1194783ae864d5cd80a85
Jonathan Humphrey <60307403+JonathanHumphrey@users.noreply.github.com>
2022-04-28 15:17:49 -0500

    Add files via upload

--- workMacro.py
+++ workMacro.py
@@ -1,24 +1,58 @@
+
 import pyautogui
 import keyboard
 
 
-def DialNext():
-    print("Dialing Next...")
-    pyautogui.moveTo(188, 225, duration=1)
-    pyautogui.click()
+def Disposition():
+    print("Select a Disposition for the call: \nAns. Mach. = '-'\nNo Answer = '*'\nDisconnected = '/'\n")
+    keyboard.read_event()
+    if keyboard.is_pressed('-'):
+        # Dispositions as Answering Machine
+        print("Answering Machine")
+        pyautogui.moveTo(669, 331)
+        pyautogui.click()
+    elif keyboard.is_pressed('*'):
+        # Dispositions as No Answer
+        print("No Answer")
+        pyautogui.moveTo(1026, 384)
+        pyautogui.click()
+    elif keyboard.is_pressed('/'):
+        # Dispositions as Disconnected
+        print("Disconnected")
+        pyautogui.moveTo(722, 502)
+        pyautogui.click()
 
 
 def HangUp():
     print("Hanging Up...")
-    pyautogui.moveTo(177, 565, duration=1)
+    pyautogui.moveTo(177, 565)
     pyautogui.click()
 
+    Disposition()
 
-print("Spooling Up...")
-while keyboard.read_key() != "*":
-    if keyboard.read_key() == "1":
-        DialNext()
-    elif keyboard.read_key() == "2":
-        HangUp()
-    elif keyboard.read_key() == "*":
-        print("Quitting Process")
+
+def DialNext():
+    print("Dialing Next...")
+    pyautogui.moveTo(188, 225)
+    pyautogui.click()
+    keyboard.wait('-')
+    HangUp()
+
+
+def main():
+    loopTracker = True
+
+    print("Spooling Up...")
+
+    while loopTracker:
+
+        print("\nPress + to Dial\nPress * to quit\n")
+        keyboard.read_event()
+        if keyboard.is_pressed('+'):
+            DialNext()
+        elif keyboard.is_pressed('*'):
+            loopTracker = False
+
+
+if __name__ == "__main__":
+    main()
